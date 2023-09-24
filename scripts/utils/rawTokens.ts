10c2712bda8e819fd106ab95b2aa3bc1509ae969
BoThe1K <bohdan@thestratos.org>
2023-09-24 12:49:18 +0200

    Add mainnet tokens

--- scripts/utils/rawTokens.ts
+++ scripts/utils/rawTokens.ts
@@ -1,7 +1,9 @@
 import { TokenInfo } from "@uniswap/token-lists";
 
-import mesos from "../../src/mesos.tokens.json";
+import ethereum from "../../src/ethereum.tokens.json";
+import stratos from "../../src/stratos.tokens.json";
 import goerli from "../../src/goerli.tokens.json";
+import mesos from "../../src/mesos.tokens.json";
 
 type IRawToken = Pick<TokenInfo, "address" | "name" | "symbol"> &
   Partial<Pick<TokenInfo, "logoURI" | "decimals">> & {
@@ -11,15 +13,22 @@ type IRawToken = Pick<TokenInfo, "address" | "name" | "symbol"> &
 
 type IRawTokenListJson = readonly IRawToken[];
 
-export const WEB3_NETWORK_NAMES = ["mesos", "goerli"] as const;
+export const WEB3_NETWORK_NAMES = [
+  "stratos",
+  "mesos",
+  "ethereum",
+  "goerli",
+] as const;
 export type IWeb3Network = typeof WEB3_NETWORK_NAMES[number];
 
 // assert the JSON is valid
 const rawTokensJson: {
   [network in IWeb3Network]: [number, IRawTokenListJson];
 } = {
-  mesos: [2047, mesos],
+  ethereum: [1, ethereum],
+  stratos: [2048, stratos],
   goerli: [5, goerli],
+  mesos: [2047, mesos],
 };
 
 export const getNetworkTokens = (network: IWeb3Network): IRawTokenListJson =>
